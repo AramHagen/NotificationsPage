02b87a11fc498f376ed64b5dc10558f4d66eb862
ZahraEm <zahra.emami7@gmail.com>
2024-02-06 06:54:41 +0330

    feat: add timeAgo pipe

--- src/app/components/notifications-list/notifications-list.component.ts
+++ src/app/components/notifications-list/notifications-list.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Notification } from '../../models/notification.model';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { TimeAgoPipe } from '../../pipes/time-ago.pipe';
 @Component({
   selector: 'app-notifications-list',
   standalone: true,
-  imports: [CommonModule,HttpClientModule],
+  imports: [CommonModule,HttpClientModule, TimeAgoPipe],
   templateUrl: './notifications-list.component.html',
   styleUrl: './notifications-list.component.scss'
 })
